012ee9d26fb22212578e6fd8a8e633c6ef7924d6
Felipe Rodrigues <felipe@felipevr.com>
2020-04-21 15:49:27 -0300

    Add project definition and draft structure

---
+++ src/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+typedef struct movie
+{
+    int id;
+    char title[50];
+    char genre[50];
+    int rooms[10];
+    char synopsis[2000];
+} movie;
+
+#endif
