b579fa4d1c9564f961f879cfd5cb537d32d0e569
Felipe Rodrigues <felipe@felipevr.com>
2020-04-21 16:22:02 -0300

    Add function headings and updates struct

--- src/functions.h
+++ src/functions.h
@@ -7,7 +7,15 @@ typedef struct movie
     char title[50];
     char genre[50];
     int rooms[10];
-    char synopsis[2000];
+    char synopsis[200];
 } movie;
+
+int create_movie(movie *m);
+int delete_movie(int movie_id);
+int list_movie_title_rooms();
+int list_movie_by_gender(char genre[50]);
+int find_title_by_id();
+int find_info_by_id();
+int list_all();
 
 #endif
