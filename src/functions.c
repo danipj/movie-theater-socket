cdbd504e1f3c162ab4bb150aa4e4112518c70a39
Daniela <d166301@dac.unicamp.br>
2020-04-21 22:38:43 -0300

    Create movie function

---
+++ src/functions.c
@@ -0,0 +1,55 @@
+#include "functions.h"
+#include <stdio.h>
+#include <string.h>
+
+char* filename = "db.txt";
+
+// int count_movies(){
+//     FILE *fp; 
+//     int count = 0;  // Line counter (result) 
+//     char c;  //
+//     FILE *f = fopen(filename, "r");
+//     if (f == NULL)
+//     {
+//         printf("Error opening file!\n");
+//         return(-1);
+//     }
+
+//     // Extract characters from file and store in character c 
+//     for (c = getc(fp); c != EOF; c = getc(fp)) 
+//         if (c == '\n') // Increment count if this character is newline 
+//             count = count + 1; 
+  
+//     // Close the file 
+//     fclose(fp); 
+//     return(count/5);
+// }
+
+/** recebe um filme 
+    int id;
+    char title[50];
+    char genre[50];
+    int room;
+    char synopsis[200];
+
+    salva o filme no arquivo de banco de dados
+
+    se correr tudo bem retorna 1, caso contrario retorna -1
+**/
+int create_movie(int id, char title[50], char genre[50], int room, char synopsis[200]){
+    FILE *f = fopen(filename, "a+");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        return(-1);
+    }
+
+    /*int id = count_movies()+1; /*descobre prox id*/
+    fprintf(f, "%d\n", id);
+    fprintf(f, "%s\n", title);
+    fprintf(f, "%s\n", genre);
+    fprintf(f, "%d\n", room);
+    fprintf(f, "%s\n", synopsis);
+
+    fclose(f);
+}
--- src/functions.h
+++ src/functions.h
@@ -6,11 +6,11 @@ typedef struct movie
     int id;
     char title[50];
     char genre[50];
-    int rooms[10];
+    int room;
     char synopsis[200];
 } movie;
 
-int create_movie(movie *m);
+int create_movie(int id, char title[50], char genre[50], int room, char synopsis[200]);
 int delete_movie(int movie_id);
 int list_movie_title_rooms();
 int list_movie_by_gender(char genre[50]);
